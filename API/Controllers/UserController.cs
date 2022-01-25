63d0245f0067156370f45f5afde8a1bd6f423605
pethaf <pethaf1@gmail.com>
2022-01-25 20:13:29 +0100

    Add support for basic authentication. This is highly insecure and is only here to get started. This should in no way shape or form be allowed to persist to release.

--- API/Controllers/UserController.cs
+++ API/Controllers/UserController.cs
@@ -1,11 +1,32 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Service.DTO;
+using System.Net.Http.Headers;
+using System.Text;
 
 namespace API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/")]
     [ApiController]
     public class UserController : ControllerBase
     {
+        [HttpPost("create")]
+        public IActionResult CreateUser([FromBody] UserDTO userDTO)
+        {
+            return Ok(); //TODO Add service to add user to actual DB should return BadRequest if userName is already in DB or if  
+        }
+        [HttpPost("login")]
+        public IActionResult LoginUser()
+        {
+            var header = AuthenticationHeaderValue.Parse(Request.Headers["Authorization"]); //This corresponds to using basic authorization in postman. Remember to turn "Enable SSL certificate verification off" under settings and select Type Basic Auth under Authorization  
+            var credentialsAsbase64 = header.Parameter;
+            byte[] data = Convert.FromBase64String(credentialsAsbase64);
+            string decodedString = Encoding.UTF8.GetString(data);
+            var splitString = decodedString.Split(":");
+            var Username = splitString[0];
+            var Password = splitString[1];
+            return Ok(); //TODO Add service to get user from DB, check that username and passwords match. Return error if not or of if no Authorization header has been provided. Should return BadRequest() in those cases, should also set UserIsLogedIn to true. otherwise should return ok. 
+        }
+
     }
 }
--- Service/DTO/UserDTO.cs
+++ Service/DTO/UserDTO.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace Service.DTO
 {
-    internal class UserDTO
+    public class UserDTO
     {
-    }
+        public string Fname { get; set; }
+        public string Lname { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; } //THis way of authing should in no way shape or form be allowed to persist to release. 
+        public string email { get; set; }
+     }
 }
