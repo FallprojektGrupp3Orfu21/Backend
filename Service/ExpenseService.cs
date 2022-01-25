8e72c12ff9822b290f328af24739773cd4165704
pethaf <pethaf1@gmail.com>
2022-01-25 22:36:09 +0100

    Add dtos and services

---
+++ Service/ExpenseService.cs
@@ -0,0 +1,28 @@
+﻿using Service.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class ExpenseService
+    {
+        public ExpenseDTO AddExpense(ExpenseDTO expense)
+        {
+            //Add to db
+            return expense;
+        }
+        public List<ExpenseDTO> GetExpensesByUser(UserDTO user)
+        {
+            var Expenses = new List<ExpenseDTO>();
+            Expenses.Add(
+                new ExpenseDTO
+                {
+
+                });
+            return Expenses;
+        }
+    }
+}
