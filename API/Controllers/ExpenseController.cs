6216a0bedbd1392dbbd7440e83f59e7b672a5432
Zeiyuri <zeyurigames@gmail.com>
2022-01-31 12:54:16 +0100

    Added Functionality for creating an Expense on a user. More clarification still needed regarding how the Date should be added (same as creation, or custom by user) partly finished solution for both cases. Currently applied same as creation. Also Changed from Float to Decimal in Expenses for easier rounding. Also Added comments to code in ExpenseCategoryService because i got confused by my own code.

---
+++ API/Controllers/ExpenseController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Service;
+using Service.Models;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace API.Controllers
+{
+    [Route("api/")]
+    [ApiController]
+    public class ExpenseController : ControllerBase
+    {
+        private UserService _userService;
+        private ExpenseService _expenseService;
+        public ExpenseController()
+        {
+            _expenseService = new ExpenseService();
+            _userService = new UserService();
+        }
+
+        [HttpPost("createExpense")]
+        public IActionResult CreateExpense([FromBody] ExpenseDTO expenseDTO)
+        {
+            var header = AuthenticationHeaderValue.Parse(Request.Headers["Authorization"]); //This corresponds to using basic authorization in postman. Remember to turn "Enable SSL certificate verification off" under settings and select Type Basic Auth under Authorization  
+            var credentialsAsbase64 = header.Parameter;
+            byte[] data = Convert.FromBase64String(credentialsAsbase64);
+            string decodedString = Encoding.UTF8.GetString(data);
+            var splitString = decodedString.Split(":");
+            var Username = splitString[0];
+            var Password = splitString[1];
+
+            
+            if (_userService.IsUserLoggedIn(Username, Password))
+            {
+                try
+                {
+                    _expenseService.AddExpense(expenseDTO, Username);
+                    return Ok(expenseDTO);
+                }
+
+                catch (Exception err)
+                {
+                    return BadRequest(err.Message);
+                }
+            }
+            else
+            {
+                return BadRequest("User not logged in");
+            }
+            
+        }
+    }
+}
--- Service/ExpenseService.cs
+++ Service/ExpenseService.cs
@@ -1,4 +1,5 @@
-﻿using Service.DTO;
+﻿using DAL.Models;
+using Service.DTO;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,10 +10,59 @@ namespace Service
 {
     public class ExpenseService
     {
-        public ExpenseDTO AddExpense(ExpenseDTO expense)
+        private ExpenseCategoryService _expenseCategoryService;
+        public ExpenseService()
         {
-            //Add to db
-            return expense;
+            _expenseCategoryService = new ExpenseCategoryService();
+        }
+        public bool AddExpense(ExpenseDTO expense, string userName)
+        {
+            using (var context = new EconomiqContext())
+            {
+                var user = context.Users.Where(user => user.UserName == userName).FirstOrDefault();
+                if (user == null)
+                {
+                    throw new Exception("No User with this Username.");
+                }
+                var category = context.ExpensesCategory.Where(c => c.CategoryName.ToLower() == expense.CategoryName.ToLower()).FirstOrDefault();
+                if (category == null)
+                {
+                    try
+                    {
+                        _expenseCategoryService.CreateExpenseCategory(userName, expense.CategoryName);
+                        category = context.ExpensesCategory.Where(c => c.CategoryName.ToLower() == expense.CategoryName.ToLower()).FirstOrDefault();
+                    }
+                    catch 
+                    {
+                        throw new Exception("Something Went Wrong Here");
+                    } 
+                }
+                if(expense.Title.Length > 50)
+                {
+                    throw new Exception("Title Too Long (Needs to be less than 50 characters)");
+                }
+                //DateTime creationDate = DateTime.Parse(expense.Date); -Read Comment on date in ExpenseDTO
+                DateTime creationDate = DateTime.Now;
+                var newExpense = new Expense { Amount = expense.Amount, ExpenseDate = creationDate, Comment = expense.Title, UserNavId = user.Id, CategoryNavId = category.Id};
+                
+                if(user.UserExpensesNav == null)
+                {
+                    user.UserExpensesNav = new List<Expense> { newExpense };
+                }
+                else
+                {
+                    user.UserExpensesNav.Add(newExpense);
+                }
+                try
+                {
+                    context.SaveChanges();
+                    return true;
+                }
+                catch
+                {
+                    throw new Exception("Something went wrong");
+                }
+            }
         }
         public List<ExpenseDTO> GetExpensesByUser(UserDTO user)
         {
