b76cbd0017d0912a27220d74d8c118f124dd9a1e
Peppo <stefan.krakowsky@studernade-skys.se>
2022-03-31 09:29:36 +0200

    Created branch and DTO for FilterExpense

---
+++ Service/DTO/FilterExpenseDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.DTO
+{
+    public class FilterExpenseDTO
+    {
+
+        public string FilteredBy { get; set; }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
